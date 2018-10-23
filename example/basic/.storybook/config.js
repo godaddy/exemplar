4e75dc80d4f7f4454b515144f44bffc47d52dda7
Dustin Savery <dsavery@godaddy.com>
2018-10-23 15:23:54 -0700

    [decorators] refactoring decorators into config.js

--- example/basic/.storybook/config.js
+++ example/basic/.storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { Decorators } from '@exemplar/react';
+import { checkA11y } from '@storybook/addon-a11y';
+
+addDecorator(checkA11y);
+addDecorator(Decorators.CenterDecorator);
 
 function loadStories() {
   require('../stories/index.js');
--- example/basic/stories/primary/index.js
+++ example/basic/stories/primary/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { checkA11y } from '@storybook/addon-a11y';
-import { Decorators } from '@exemplar/react';
 import './index.css';
 
 export default storiesOf('Buttons', module)
-  .addDecorator(Decorators.CenterDecorator)
-  .addDecorator(checkA11y)
   .add('Primary Action', () => {
     return <button className='primaryBtn'>Primary</button>;
   });
