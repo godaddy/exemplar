7a771cac7fe6f11a35793fa77437cda680c90ba3
Dustin Savery <dsavery@godaddy.com>
2018-10-23 14:54:22 -0700

    [fix] fixing addons

--- example/basic/stories/secondary/index.js
+++ example/basic/stories/secondary/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Decorators } from '@exemplar/react';
+import { checkA11y } from '@storybook/addon-a11y';
 import './index.css';
 
 export default storiesOf('Buttons', module)
   .addDecorator(Decorators.CenterDecorator)
+  .addDecorator(checkA11y)
   .add('Secondary Action', () => {
     return <button className='secondaryBtn'>Secondary</button>;
   });
