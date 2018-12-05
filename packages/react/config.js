5d16f25502c3507087ccb955a2ba21988d1b5928
Charlie Robbins <crobbins@godaddy.com>
2018-12-05 15:55:03 -0500

    [fix api] Define EXEMPLAR_ROOT using DefinePlugin.

--- packages/react/config.js
+++ packages/react/config.js
@@ -1,9 +1,8 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-const path = require('path');
 
 function loadStories() {
-  require(path.join(process.cwd(), 'stories', 'index.js');
+  require(EXEMPLAR_ROOT);
 }
 
 addDecorator(withKnobs);
