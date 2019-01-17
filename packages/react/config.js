66ce22f28f6fd81f994281db1fe84b648bf7d430
Charlie Robbins <crobbins@godaddy.com>
2019-01-17 14:45:11 -0500

    [api] Make it so. Engage.

--- packages/react/config.js
+++ packages/react/config.js
@@ -1,8 +1,19 @@
-import { configure, addDecorator } from '@storybook/react';
+import {
+  configure,
+  storiesOf,
+  addDecorator
+} from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 
+const stories = storiesOf(EXEMPLAR_NAME, module);
+
 function loadStories() {
-  require(EXEMPLAR_ROOT);
+  [EXEMPLAR_STORIES, EXEMPLAR_STORIES_WEB].forEach(list => {
+    JSON.parse(list).forEach(file => {
+      const Component = require(file);
+      stories.add(file, () => <Component />);
+    });
+  });
 }
 
 addDecorator(withKnobs);
--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -1,10 +1,32 @@
 const { DefinePlugin } = require('webpack');
 const path = require('path');
 
+
+const examplesDir = path.join(process.cwd(), 'examples');
+const packageJson = require(path.join(process.cwd(), 'package.json'));
+
+function nonIndexJs(file) {
+  return path.extname(file) === '.js'
+    && file !== 'index.js';
+}
+
+function getExamples(dir) {
+  const files = fs.readdirSync(dir)
+    .filter(nonIndexJs)
+    .map(file => path.join(dir, file))
+
+  return JSON.stringify(files);
+}
+
 module.exports = {
   plugins: [
     new DefinePlugin({
-      EXEMPLAR_ROOT: `'${path.join(process.cwd(), 'stories', 'index.js')}'`
+      // Filter out directories and index.js
+      EXEMPLAR_STORIES: getExamples(examplesDir),
+      WEB_STORIES: getExamples(path.join(examplesDir, 'web')),
+      //NATIVE_STORIES: fs.readdirSync(path.join(examplesDir, 'native'))
+      EXEMPLAR_ROOT: `'${path.join(process.cwd(), 'stories', 'index.js')}'`,
+      EXEMPLAR_NAME: JSON.stringify(packageJson.name)
     })
   ],
   module: {
