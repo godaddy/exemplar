3300559f24bd9fe957186216bd022a6db7cd64c5
Sivan Mehta <smehta@godaddy.com>
2019-01-18 11:17:12 -0800

    [bit] remove unneeded files

--- packages/react/stories/index.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Globals', module)
-  .add('EXEMPLAR_ROOT', () => <pre>{ EXEMPLAR_ROOT }</pre>)
-  .add('LIST_OF_EXAMPLES', () => <pre>{ JSON.stringify(LIST_OF_EXAMPLES, null, 2) }</pre>)
