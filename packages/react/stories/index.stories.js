e9175f6c11d4bcba0a1ffc05a7d5416ea0f325cc
Sivan Mehta <smehta@godaddy.com>
2019-01-18 11:15:45 -0800

    [wip] it almost works

--- packages/react/stories/index.stories.js
+++ packages/react/stories/index.stories.js
@@ -1,21 +1,6 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
+storiesOf('Globals', module)
   .add('EXEMPLAR_ROOT', () => <pre>{ EXEMPLAR_ROOT }</pre>)
-  .add('LIST_OF_EXAMPLES', () => <pre>{ LIST_OF_EXAMPLES }</pre>)
+  .add('LIST_OF_EXAMPLES', () => <pre>{ JSON.stringify(LIST_OF_EXAMPLES, null, 2) }</pre>)
