c1840e9302485f272386e7e7cfddf3d2c25e7ed0
Charlie Robbins <crobbins@godaddy.com>
2019-01-23 03:02:31 -0500

    [refactor] Use functional webpack style for Storybook. [api] Load env/shared.scss if it exists. [api] Provide an empty string if a given directory does not exist.

--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -2,28 +2,93 @@ const { DefinePlugin } = require('webpack');
 const path = require('path');
 const fs = require('fs');
 
+/**
+ * Resolves the given request for a module.
+ *
+ * @param {String} request - The requested module path.
+ * @param {Boolean} required - Whether or not the module is required.
+ * @returns {String?} - The requested string, or null if the module was not required and not found.
+ */
+function resolveModule(request, required) {
+  try {
+    return require.resolve(request);
+  } catch (err) {
+    if (required || err.code !== 'MODULE_NOT_FOUND') {
+      console.error(`Error resolving request ${request}`);
+      throw err;
+    }
+  }
+
+  return null;
+}
+
+/**
+ * Resolves the given request for a directory.
+ *
+ * @param {String} request - The requested dir path.
+ * @param {Boolean} required - Whether or not the directory is required.
+ * @returns {String?} - The requested string, or null if the directory was not required and not found.
+ */
+function resolveDir(request, required) {
+  try {
+    const stats = fs.statSync(request);
+    return `'${request}'`;
+  } catch (err) {
+    if (required || err.code !== 'ENOENT') {
+      console.error(`Error resolving directory ${request}`);
+      throw err;
+    }
+  }
+
+  return `''`;
+}
+
+
 const cwd = process.cwd();
 const rootDir = path.join(cwd, 'examples');
+const pkg = require(path.join(cwd, 'package.json'));
 
 const dirs = {
   root: rootDir,
   crossPlatform: rootDir,
   webOnly: path.join(rootDir, 'web'),
-  scss: path.join(rootDir, 'scss')
+  env: path.join(rootDir, 'env')
 };
 
-module.exports = {
-  plugins: [
-    new DefinePlugin({
-      EX_CROSS_PLATFORM: `'${dirs.crossPlatform}'`,
-      EX_WEB: `'${dirs.webOnly}'`,
+//
+// TODO (@indexzero): strongly consider renaming
+// this from `examples/env` to `examples/shared`!
+//
+const env = {
+  scss: resolveModule(path.join(dirs.env, 'shared.scss'))
+};
+
+module.exports = function (baseConfig, envName, webpackConfig) {
+  const definitions = {
+    EX_CROSS_PLATFORM: resolveDir(dirs.crossPlatform),
+    EX_WEB: resolveDir(dirs.webOnly),
+    EX_PKG_JSON: JSON.stringify({
+      name: pkg.name,
+      version: pkg.version
     })
-  ],
-  module: {
-    rules: [{
+  };
+
+  //
+  // Load SCSS when applicable
+  // TODO (@indexzero): Make this configurable
+  //
+  if (env.scss) {
+    definitions.EX_ENV_SCSS = `'${env.scss}'`;
+
+    webpackConfig.module.rules.push({
       test: /\.scss$/,
       loaders: ['style-loader', 'css-loader', 'sass-loader'],
       include: dirs.root
-    }]
+    });
   }
+
+  // Define the paths that exemplar will attempt to load
+  webpackConfig.plugins.push(new DefinePlugin(definitions));
+
+  return webpackConfig;
 };
