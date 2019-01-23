57b0a165c93dbd6a532c22c162d5be18a49d83bb
Charlie Robbins <crobbins@godaddy.com>
2019-01-23 02:08:21 -0500

    [refactor] Tighten it up.

--- packages/react/.storybook/webpack.config.js
+++ packages/react/.storybook/webpack.config.js
@@ -1,31 +1,4 @@
-const { DefinePlugin } = require('webpack');
-const path = require('path');
-const fs = require('fs');
-
-const cwd = process.cwd();
-
-function listOfExamples() {
-  return fs.readdirSync(path.join(cwd, 'examples'))
-    .filter(f => f.match(/.js$/))
-    .map(f => path.join(cwd, f))
-}
-
-module.exports = {
-  plugins: [
-    new DefinePlugin({
-      EXEMPLAR_ROOT: `'${path.join(cwd, 'stories', 'index.js')}'`,
-      EXAMPLES_DIRECTORY: `'${path.join(cwd, 'examples')}'`,
-      WEB_ONLY_DIRECTORY: `'${path.join(cwd, 'examples', 'web')}'`,
-      LIST_OF_EXAMPLES: JSON.stringify(listOfExamples())
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: process.cwd()
-      }
-    ]
-  }
-};
+//
+// Shim publicly exposed module
+//
+module.exports = require('../webpack.config.js');
--- packages/react/add-all-examples.js
+++ packages/react/add-all-examples.js
@@ -6,16 +6,28 @@ import { storiesOf } from '@storybook/react';
  * https://github.com/webpack/docs/wiki/context#context-module-api
  *
  * @param  {¯\_(ツ)_/¯} context some wizardy that require.context returns
- * @return {React.Component[]} A list of React.Components that can be rendered
+ * @return {Example[]} A list of React.Components that can be rendered
  */
 function requireAll(context) {
-  return context.keys().map(context).map(C => C.default);
+  return context.keys().map(key => {
+    const mod = context(key);
+    return {
+      source: key,
+      Component: mod.default
+    };
+  });
 }
-const crossPlatform = requireAll(require.context(EXAMPLES_DIRECTORY, true, /^\.\/.*\.js$/));
-const webOnly = requireAll(require.context(WEB_ONLY_DIRECTORY, true, /^\.\/.*\.js$/));
+
+const crossPlatform = requireAll(require.context(EX_CROSS_PLATFORM, true, /^\.\/.*\.js$/));
+const webOnly = requireAll(require.context(EX_WEB, true, /^\.\/.*\.js$/));
 
 const modules = crossPlatform.concat(webOnly);
 
-modules.reduce((story, Component, i) => {
-  return story.add(Component.name, () => <Component>wat</Component>)
+modules.reduce((stories, example, i) => {
+  const { source, Component } = example;
+
+  // Do not attempt to load invalid or undefined example Components
+  if (!Component || !Component.name) return stories;
+
+  return stories.add(Component.name, () => React.createElement(Component));
 }, storiesOf('Dynamically Loaded Examples', module));
