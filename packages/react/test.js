ed7d8bc827b8d728f34cd5d064713a94f25ecbcd
Charlie Robbins <crobbins@godaddy.com>
2019-01-29 14:28:49 -0500

    [test] Basic smoke test that ensures the webpack build passes.

---
+++ packages/react/test.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const rimraf = require('rimraf');
+const fs = require('fs');
+const assume = require('assume');
+const { spawn } = require('child_process');
+
+const outDir = path.join(__dirname, '.testout');
+
+function assumeBuildStorybook(config) {
+  return done => {
+    const storybook = spawn('build-storybook', [
+      '-c', config,
+      '-o', './.testout'
+    ]);
+
+    if (process.env.DEBUG) {
+      storybook.stdout.pipe(process.stdout);
+      storybook.stderr.pipe(process.stderr);
+    }
+
+    storybook.once('error', done);
+    storybook.on('exit', code => {
+      assume(code).equals(0);
+      done();
+    });
+  };
+}
+
+describe('@exemplar/storybook', function () {
+  this.timeout(15 * 1000);
+
+  beforeEach(done => {
+    rimraf(outDir, done.bind(null, null));
+  });
+
+  it('build-storybook -c ./ should work correctly', assumeBuildStorybook('./'));
+  it('build-storybook -c ./.storybook should work correctly', assumeBuildStorybook('./.storybook'));
+});
