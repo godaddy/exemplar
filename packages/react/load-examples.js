6fac913c3da037daf3775cd3ad6276535c865b96
Charlie Robbins <crobbins@godaddy.com>
2019-01-30 15:55:43 -0500

    [fix] Correct loading regular expressions. Ignore index.js (for now).

--- packages/react/load-examples.js
+++ packages/react/load-examples.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+const isIndex = /index\.js/;
+
 /**
  * Generate module from the given context. Taken directly from the wiki here
  * https://github.com/webpack/docs/wiki/context#context-module-api
@@ -8,23 +10,25 @@ import { storiesOf } from '@storybook/react';
  * @param  {webpack.require} context some wizardry that require.context returns
  * @returns {Example[]} A list of React.Components that can be rendered
  */
-function requireAll(context) {
+function requireAll(context, i) {
   return context.keys().map(key => {
+    if (isIndex.test(key)) return;
+
     const mod = context(key);
     return {
       source: key,
       Component: mod.default
     };
-  });
+  }).filter(Boolean);
 }
 
 // eslint-disable-next-line no-undef
 const pkg = EX_PKG_JSON;
 const contexts = [
   // eslint-disable-next-line no-undef
-  EX_CROSS_PLATFORM && require.context(EX_CROSS_PLATFORM, true, /^([a-zA-Z-_]+)\.js$/),
+  EX_CROSS_PLATFORM && require.context(EX_CROSS_PLATFORM, false, /^\.\/([-\w]+)\.js$/),
   // eslint-disable-next-line no-undef
-  EX_WEB && require.context(EX_WEB, true, /^([a-zA-Z-_]+)\.js$/)
+  EX_WEB && require.context(EX_WEB, true, /^\.\/([-\w]+)\.js$/)
 ].filter(Boolean);
 
 const modules = contexts.reduce((all, context) => {
@@ -41,11 +45,11 @@ try {
   console.warn(ex);
 }
 
-modules.reduce((stories, example, i) => {
+modules.reduce((stories, example) => {
   const { Component } = example;
 
   // Do not attempt to load invalid or undefined example Components
   if (!Component || !Component.name) return stories;
 
-  return stories.add(`[${i}] ${Component.name}`, () => React.createElement(Component));
+  return stories.add(`${Component.name}`, () => React.createElement(Component));
 }, storiesOf(pkg.name, module));
