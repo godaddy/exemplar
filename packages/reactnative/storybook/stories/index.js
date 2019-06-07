0f4b5f324f7dbec550cd65399010b77c5c6405b2
Sivan Mehta <smehta@godaddy.com>
2019-06-07 13:15:21 -0700

    [doc] remove unused files

--- packages/reactnative/storybook/stories/index.js
+++ packages/reactnative/storybook/stories/index.js
@@ -5,10 +5,6 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Button from './Button';
-import CenterView from './CenterView';
-import Welcome from './Welcome';
-
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
