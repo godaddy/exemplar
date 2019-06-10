65b9c280425154eca32883cba07481dfc1510d9c
Sivan Mehta <smehta@godaddy.com>
2019-06-10 10:45:11 -0700

    everything but reaching out of node_modules

---
+++ packages/reactnative/examples/native/emojis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+
+export default () => (
+  <View style={{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }}>
+    <Button title='😀😎👍💯' />
+  </View>
+);
--- packages/reactnative/storybook/stories/index.js
+++ packages/reactnative/storybook/stories/index.js
@@ -1,34 +1,43 @@
+// import React from 'react';
+// import { Button, View } from 'react-native';
+
+// import { storiesOf } from '@storybook/react-native';
+// import { action } from '@storybook/addon-actions';
+
+// const Center = ({ children }) => (
+//   <View style={{
+//     flex: 1,
+//     flexDirection: 'column',
+//     justifyContent: 'center'
+//   }}>
+//     { children }
+//   </View>
+// )
+
+// storiesOf('Button', module)
+//   .add('with text', () => (
+//     <Center>
+//       <Button
+//         title='hi there'
+//         onPress={action('clicked-text')}>
+//       </Button>
+//      </Center>
+//   ))
+//   .add('with some emoji', () => (
+//     <Center>
+//       <Button
+//         title='😀 😎 👍 💯'
+//         onPress={action('clicked-emoji')}>
+//       </Button>
+//     </Center>
+//   ));
+
 import React from 'react';
-import { Button, View } from 'react-native';
-
 import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-const Center = ({ children }) => (
-  <View style={{
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center'
-  }}>
-    { children }
-  </View>
-)
+import Component0 from '../../../../examples/native/emojis';
+import Component1 from '../../../../examples/native/text';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Center>
-      <Button
-        title='hi there'
-        onPress={action('clicked-text')}>
-      </Button>
-     </Center>
-  ))
-  .add('with some emoji', () => (
-    <Center>
-      <Button
-        title='😀 😎 👍 💯'
-        onPress={action('clicked-emoji')}>
-      </Button>
-    </Center>
-  ));
+storiesOf('Exemplar', module)
+.add('examples/native/emojis.js', () => <Component0 />)
+.add('examples/native/text.js', () => <Component1 />);
