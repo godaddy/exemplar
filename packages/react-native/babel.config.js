6e5436c9f5f522202eb75215ad1c6e475e9a2650
Martijn Swaagman <info@martijnswaagman.nl>
2019-06-25 00:52:24 +0200

    [major] switch back to metro, bye bye haul

--- packages/react-native/babel.config.js
+++ packages/react-native/babel.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [[
+      'module-resolver', {
+        alias: {
+          examples: '/Users/mswaagman/projects/ux-button/examples'
+        }
+      }
+    ]]
+  };
 };
