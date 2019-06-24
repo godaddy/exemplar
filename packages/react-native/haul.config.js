b394feeff7425b2422952e2b5ea55a89849c8bdc
Martijn Swaagman <info@martijnswaagman.nl>
2019-06-24 16:22:37 +0200

    [fix] configs

--- packages/react-native/haul.config.js
+++ packages/react-native/haul.config.js
@@ -1,7 +1,26 @@
 import { createWebpackConfig } from 'haul';
+import path from 'path';
 
 export default {
-  webpack: createWebpackConfig(() => ({
-    entry: './index.js'
-  }))
+  webpack: env => {
+    const config = createWebpackConfig({
+      entry: './index.js'
+    })(env);
+
+    config.module.rules = [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve('node_modules/react-native/'),
+          path.resolve('node_modules/metro/'),
+          path.resolve('node_modules/react-devtools-core/'),
+          path.resolve('node_modules/react-hot-loader/')
+        ],
+        loaders: ['babel-loader']
+      },
+      ...config.module.rules
+    ];
+
+    return config;
+  }
 };
