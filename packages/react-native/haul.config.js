e44f5a44784cdaa488c6b935f35cfd4b71f9e83b
Martijn Swaagman <info@martijnswaagman.nl>
2019-06-24 16:04:46 +0200

    [fix] style haul config

--- packages/react-native/haul.config.js
+++ packages/react-native/haul.config.js
@@ -1,7 +1,7 @@
-import { createWebpackConfig } from "haul";
+import { createWebpackConfig } from 'haul';
 
 export default {
-  webpack: createWebpackConfig(({ platform }) => ({
-    entry: `./index.js`
+  webpack: createWebpackConfig(() => ({
+    entry: './index.js'
   }))
 };
