6b05c83b4e8ffc35813439dbc88af0c5a18fe27b
Martijn Swaagman <info@martijnswaagman.nl>
2019-06-25 08:08:51 +0200

    [fix] default babel config

--- packages/react-native/babel.config.js
+++ packages/react-native/babel.config.js
@@ -1,14 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [[
-      'module-resolver', {
-        alias: {
-          examples: '/Users/mswaagman/projects/ux-button/examples'
-        }
-      }
-    ]]
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset']
 };
