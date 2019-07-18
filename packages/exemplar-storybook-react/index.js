1c9141a3a942f01d80bdcc45226a92c7a6e61241
indexzero <charlie.robbins@gmail.com>
2019-07-18 07:00:54 -0400

    [refactor] Backwards compatible implementation based entirely on the on new presets API.

---
+++ packages/exemplar-storybook-react/index.js
@@ -0,0 +1,122 @@
+const debug = require('diagnostics')('exemplar:preset');
+const { DefinePlugin } = require('webpack');
+const {
+  alias,
+  dirs,
+  setup,
+  definitions
+} = require('./constants');
+
+/**
+ * Extend the Storybook manager webpack configuration
+ * @param  {Object} baseConfig Webpack configuration to modify
+ * @param  {Object} options    Settings provided by Storybook
+ * @returns {Object} Modified webpack configuration.
+ */
+exports.managerWebpack = async function managerWebpack(baseConfig) {
+  const { plugins = [] } = baseConfig;
+
+  return {
+    ...baseConfig,
+    plugins: [
+      ...plugins,
+      // Define the paths that exemplar will attempt to load
+      new DefinePlugin(definitions)
+    ]
+  };
+};
+
+/**
+ * Extend the Storybook preview webpack configuration
+ * @param  {Object} baseConfig Webpack configuration to modify
+ * @param  {Object} options    Settings provided by Storybook
+ * @returns {Object} Merged webpack configuration.
+ */
+exports.webpack = function webpack(baseConfig) {
+  const {
+    module = {},
+    plugins = [],
+    resolve = {}
+  } = baseConfig;
+  let rules = (module.rules || []).slice();
+
+  //
+  // Load SCSS when applicable
+  // TODO (@indexzero): Make this configurable through the SCSS preset
+  // https://github.com/storybookjs/presets/tree/master/packages/preset-scss
+  //
+  if (setup.scss !== `''`) {
+    debug('SCSS enabled', dirs.root);
+    rules = [
+      ...(module.rules || []),
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: dirs.root
+      }
+    ];
+  }
+
+  return {
+    ...baseConfig,
+    module: {
+      ...module,
+      rules
+    },
+    plugins: [
+      ...plugins,
+      // Define the paths that exemplar will attempt to load
+      new DefinePlugin(definitions)
+    ],
+    resolve: {
+      ...resolve,
+      // Use any aliases that may be provided
+      alias
+    }
+  };
+};
+
+
+exports.config = function config(entry = []) {
+  // Always include our default config for loading examples
+  const allConfigs = [require.resolve('./load-examples')];
+
+  //
+  // Attempt to load any user-defined configs
+  // (e.g. for setting up Storybook addons).
+  //
+  // Remark (indexzero): could optionally attempt
+  // to load `configDir/config.js` here to be less
+  // confusing to newcomers more familiar with Storybook
+  // itself.
+  //
+  let fullpath = definitions.EX_SETUP_CONFIG;
+  try {
+    let config = require.resolve(fullpath);
+    allConfigs.unshift(config);
+  } catch (ex) {
+    debug(`Error loading config ${fullpath}:`, ex);
+  }
+
+  return [...entry, ...allConfigs];
+}
+
+exports.addons = function addons(entry = []) {
+  let addons;
+
+  //
+  // Remark (indexzero): could optionally attempt
+  // to load `configDir/addons.js` here to be less
+  // confusing to newcomers more familiar with Storybook
+  // itself.
+  //
+  let fullpath = definitions.EX_SETUP_ADDONS;
+  try {
+    addons = require.resolve(fullpath);
+  } catch (ex) {
+    debug(`Error loading addons ${fullpath}:`, ex);
+    return entry;
+  }
+
+  return [...entry, addons];
+}
--- packages/exemplar-storybook-react/load-examples.js
+++ packages/exemplar-storybook-react/load-examples.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { configure, storiesOf } from '@storybook/react';
 
 /**
  * Simple function to match index.native?.js
@@ -31,34 +31,38 @@ function requireAll(context) {
   }).filter(Boolean);
 }
 
-// eslint-disable-next-line no-undef
-const pkg = EX_PKG_JSON;
-const contexts = [
+function loadExamples() {
   // eslint-disable-next-line no-undef
-  EX_CROSS_PLATFORM && require.context(EX_CROSS_PLATFORM, false, /^\.\/([-\w]+)\.js$/),
-  // eslint-disable-next-line no-undef
-  EX_WEB && require.context(EX_WEB, true, /^\.\/([-\w]+)\.js$/)
-].filter(Boolean);
+  const pkg = EX_PKG_JSON;
+  const contexts = [
+    // eslint-disable-next-line no-undef
+    EX_CROSS_PLATFORM && require.context(EX_CROSS_PLATFORM, false, /^\.\/([-\w]+)\.js$/),
+    // eslint-disable-next-line no-undef
+    EX_WEB && require.context(EX_WEB, true, /^\.\/([-\w]+)\.js$/)
+  ].filter(Boolean);
 
-const modules = contexts.reduce((all, context) => {
-  all.push.apply(all, requireAll(context));
-  return all;
-}, []);
+  const modules = contexts.reduce((all, context) => {
+    all.push.apply(all, requireAll(context));
+    return all;
+  }, []);
 
-try {
-  // eslint-disable-next-line no-undef
-  if (EX_SETUP_SCSS) require(EX_SETUP_SCSS);
-} catch (ex) {
-  // Ignore errors.
-  // eslint-disable-next-line no-console
-  console.warn(ex);
+  try {
+    // eslint-disable-next-line no-undef
+    if (EX_SETUP_SCSS) require(EX_SETUP_SCSS);
+  } catch (ex) {
+    // Ignore errors.
+    // eslint-disable-next-line no-console
+    console.warn(ex);
+  }
+
+  modules.reduce((stories, example) => {
+    const { Component } = example;
+
+    // Do not attempt to load invalid or undefined example Components
+    if (!Component || !Component.name) return stories;
+
+    return stories.add(`${Component.name}`, () => React.createElement(Component));
+  }, storiesOf(pkg.name, module));
 }
 
-modules.reduce((stories, example) => {
-  const { Component } = example;
-
-  // Do not attempt to load invalid or undefined example Components
-  if (!Component || !Component.name) return stories;
-
-  return stories.add(`${Component.name}`, () => React.createElement(Component));
-}, storiesOf(pkg.name, module));
+configure(loadExamples, module);
